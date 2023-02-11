976bac44155386ca010c13953dff669e16d07b37
belaletech <belalahmadswnbhr@gmail.com>
2023-02-11 14:46:21 +0530

    List Data type access by index value

---
+++ Datatype.py/list.py
@@ -0,0 +1,9 @@
+# list in python
+#Collection of data of different data type
+student_Name=["Belal","Ayan","Shazeb"]
+Student_Roll_No=[1,2,3]
+Student_Percentage=[60.5,77.8,99.9]
+Student_Name_and_Roll_no=["Nehal",8,99.6]
+print("Name of Studen=",student_Name[0],"Roll No=",Student_Roll_No[0],"Percentage=",Student_Percentage[0])
+print("Name of Studen=",student_Name[1],"Roll No=",Student_Roll_No[1],"Percentage=",Student_Percentage[1])
+print("Name of Studen=",student_Name[2],"Roll No=",Student_Roll_No[2],"Percentage=",Student_Percentage[2])
