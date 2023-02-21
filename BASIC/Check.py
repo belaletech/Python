289f109e5b1348e76d52f5a86c5c2a56a17af6dd
belaletech <belalahmadswnbhr@gmail.com>
2023-02-21 16:20:35 +0530

    updated

---
+++ BASIC/Check.py
@@ -0,0 +1,9 @@
+ch=input("Enter any single character = ")
+if ch.isalpha():
+    print("Alphabet")
+elif ch.isdigit():
+    print("Given input is digit")
+else:
+    print("Given input is Special character")
+
+  
