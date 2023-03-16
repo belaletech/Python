de31b22f9f351acf7a9d285e7109ada452ea759e
belaletech <belalahmadswnbhr@gmail.com>
2023-03-16 23:55:23 +0530

    update

---
+++ Galgotias University/control Structure/if_else.py
@@ -0,0 +1,15 @@
+a=int(input("Enter your percentage"))
+
+if a>=60 and a<100:
+    print("Congratulation your division is 1st")
+
+elif a>=45: 
+    print("your division is 2nd")
+elif a>100:
+    print("invalid percentage")
+elif a>=33:
+    print("Your division is 3rd")
+else:
+    print("sorry you fail")
+
+
