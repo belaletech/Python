fc4ad5423a4d66b5821037df22556be15b26c806
belaletech <belalahmadswnbhr@gmail.com>
2023-05-17 15:16:27 +0530

    update

---
+++ OsModule/OsName.py
@@ -0,0 +1,9 @@
+import os
+print(os.name)
+
+#os.mkdir("shumbham")
+#print(dir(os))
+print("Curerent directory=",os.getcwd())
+os.chdir("D:\\")
+print("Curerent directory:=",os.getcwd())
+#os.rmdir("C:\Users\belal\GU PYTHON\Python\shubham")
